18bd714455c26529add78bc802985f07721bcde0
GalBrandwine <gal080592@gmail.com>
2019-08-07 18:12:19 +0300

    Working on MQ2

--- BabySleeper/BabySleeper.ino
+++ BabySleeper/BabySleeper.ino
@@ -1136,27 +1136,59 @@
 #include <Adafruit_Sensor.h>
 //#include <Adafruit_BME280.h>
 
+
+
+
+
 #include "DHT.h"
- 
 #define DHTPIN 4
 //our sensor is DHT22 type
 #define DHTTYPE DHT22
 //create an instance of DHT sensor
 DHT dht(DHTPIN, DHTTYPE);
 
-/*
-  #include <SPI.h>
-  #define BME_SCK 18
-  #define BME_MISO 19
-  #define BME_MOSI 23
-  #define BME_CS 5
-*/
+//**************************************************************************************
+//* MQ-2 definitions                                                                   *
+//**************************************************************************************
+//const int calibrationLed = 13;                      //when the calibration start , LED pin 13 will light up , off when finish calibrating
+const int MQ_PIN=16;                                //define which analog input channel you are going to use
+int RL_VALUE=5;                                     //define the load resistance on the board, in kilo ohms
+float RO_CLEAN_AIR_FACTOR=9.83;                     //RO_CLEAR_AIR_FACTOR=(Sensor resistance in clean air)/RO,
+                                                    //which is derived from the chart in datasheet
+ 
+/***********************Software Related Macros************************************/
+int CALIBARAION_SAMPLE_TIMES=50;                    //define how many samples you are going to take in the calibration phase
+int CALIBRATION_SAMPLE_INTERVAL=500;                //define the time interal(in milisecond) between each samples in the
+                                                    //cablibration phase
+int READ_SAMPLE_INTERVAL=50;                        //define how many samples you are going to take in normal operation
+int READ_SAMPLE_TIMES=5;                            //define the time interal(in milisecond) between each samples in 
+                                                    //normal operation
+ 
+/**********************Application Related Macros**********************************/
+#define         GAS_LPG             0   
+#define         GAS_CO              1   
+#define         GAS_SMOKE           2    
+ 
+/*****************************Globals***********************************************/
+float           LPGCurve[3]  =  {2.3,0.21,-0.47};   //two points are taken from the curve. 
+                                                    //with these two points, a line is formed which is "approximately equivalent"
+                                                    //to the original curve. 
+                                                    //data format:{ x, y, slope}; point1: (lg200, 0.21), point2: (lg10000, -0.59) 
+float           COCurve[3]  =  {2.3,0.72,-0.34};    //two points are taken from the curve. 
+                                                    //with these two points, a line is formed which is "approximately equivalent" 
+                                                    //to the original curve.
+                                                    //data format:{ x, y, slope}; point1: (lg200, 0.72), point2: (lg10000,  0.15) 
+float           SmokeCurve[3] ={2.3,0.53,-0.44};    //two points are taken from the curve. 
+                                                    //with these two points, a line is formed which is "approximately equivalent" 
+                                                    //to the original curve.
+                                                    //data format:{ x, y, slope}; point1: (lg200, 0.53), point2: (lg10000,  -0.22)                                                     
+float           Ro           =  10;                 //Ro is initialized to 10 kilo ohms
 
-//Adafruit_BME280 bme; // I2C
-//Adafruit_BME280 bme(BME_CS); // hardware SPI
-//Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK); // software SPI
 
-// Replace with your network credentials
+//**************************************************************************************
+//*                                 W I F I                                            *
+//* Replace with your network credentials                                              *
+//**************************************************************************************
 const char* ssid = "gozal_2.4";
 const char* password = "asdffdsa";
 
@@ -1195,6 +1227,98 @@ String readDHTTemperature() {
   }
 }
 
+/****************** MQResistanceCalculation ****************************************
+Input:   raw_adc - raw value read from adc, which represents the voltage
+Output:  the calculated sensor resistance
+Remarks: The sensor and the load resistor forms a voltage divider. Given the voltage
+         across the load resistor and its resistance, the resistance of the sensor
+         could be derived.
+************************************************************************************/ 
+float MQResistanceCalculation(int raw_adc)
+{
+  return ( ((float)RL_VALUE*(1023-raw_adc)/raw_adc));
+}
+ 
+/***************************** MQCalibration ****************************************
+Input:   mq_pin - analog channel
+Output:  Ro of the sensor
+Remarks: This function assumes that the sensor is in clean air. It use  
+         MQResistanceCalculation to calculates the sensor resistance in clean air 
+         and then divides it with RO_CLEAN_AIR_FACTOR. RO_CLEAN_AIR_FACTOR is about 
+         10, which differs slightly between different sensors.
+************************************************************************************/ 
+float MQCalibration(int mq_pin)
+{
+  int i;
+  float val=0;
+
+  for (i=0;i<CALIBARAION_SAMPLE_TIMES;i++) {            //take multiple samples
+    val += MQResistanceCalculation(analogRead(mq_pin));
+    delay(CALIBRATION_SAMPLE_INTERVAL);
+  }
+  val = val/CALIBARAION_SAMPLE_TIMES;                   //calculate the average value
+  val = val/RO_CLEAN_AIR_FACTOR;                        //divided by RO_CLEAN_AIR_FACTOR yields the Ro                                        
+  return val;                                                      //according to the chart in the datasheet 
+
+}
+ 
+/*****************************  MQRead *********************************************
+Input:   mq_pin - analog channel
+Output:  Rs of the sensor
+Remarks: This function use MQResistanceCalculation to caculate the sensor resistenc (Rs).
+         The Rs changes as the sensor is in the different consentration of the target
+         gas. The sample times and the time interval between samples could be configured
+         by changing the definition of the macros.
+************************************************************************************/ 
+float MQRead(int mq_pin)
+{
+  int i;
+  float rs=0;
+ 
+  for (i=0;i<READ_SAMPLE_TIMES;i++) {
+    rs += MQResistanceCalculation(analogRead(mq_pin));
+    delay(READ_SAMPLE_INTERVAL);
+  }
+ 
+  rs = rs/READ_SAMPLE_TIMES;
+ 
+  return rs;  
+}
+ 
+/*****************************  MQGetGasPercentage **********************************
+Input:   rs_ro_ratio - Rs divided by Ro
+         gas_id      - target gas type
+Output:  ppm of the target gas
+Remarks: This function passes different curves to the MQGetPercentage function which 
+         calculates the ppm (parts per million) of the target gas.
+************************************************************************************/ 
+long MQGetGasPercentage(float rs_ro_ratio, int gas_id)
+{
+  if ( gas_id == GAS_LPG ) {
+     return MQGetPercentage(rs_ro_ratio,LPGCurve);
+  } else if ( gas_id == GAS_CO ) {
+     return MQGetPercentage(rs_ro_ratio,COCurve);
+  } else if ( gas_id == GAS_SMOKE ) {
+     return MQGetPercentage(rs_ro_ratio,SmokeCurve);
+  }    
+ 
+  return 0;
+}
+ 
+/*****************************  MQGetPercentage **********************************
+Input:   rs_ro_ratio - Rs divided by Ro
+         pcurve      - pointer to the curve of the target gas
+Output:  ppm of the target gas
+Remarks: By using the slope and a point of the line. The x(logarithmic value of ppm) 
+         of the line could be derived if y(rs_ro_ratio) is provided. As it is a 
+         logarithmic coordinate, power of 10 is used to convert the result to non-logarithmic 
+         value.
+************************************************************************************/ 
+long  MQGetPercentage(float rs_ro_ratio, float *pcurve)
+{
+  return (pow(10,( ((log10(rs_ro_ratio)-pcurve[1])/pcurve[2]) + pcurve[0])));
+}
+
 String readDHTHumidity() {
   float h = dht.readHumidity();
   //float h = bme.readHumidity();
@@ -1206,6 +1330,45 @@ String readDHTHumidity() {
     Serial.println(h);
     return String(h);
   }
+}
+
+String readMq2() {
+  
+  //int analogSensor = analogRead(smokeA0);
+
+  //Serial.print("MQ2 A0: ");
+  //Serial.println(analogSensor);
+  long iPPM_LPG = 0;
+  long iPPM_CO = 0;
+  long iPPM_Smoke = 0;
+  
+  iPPM_LPG = MQGetGasPercentage(MQRead(MQ_PIN)/Ro,GAS_LPG);
+  iPPM_CO = MQGetGasPercentage(MQRead(MQ_PIN)/Ro,GAS_CO);
+  iPPM_Smoke = MQGetGasPercentage(MQRead(MQ_PIN)/Ro,GAS_SMOKE);
+  
+  
+
+   Serial.println("Concentration of gas ");
+
+   Serial.print("LPG: ");Serial.print(iPPM_LPG);Serial.println(" ppm");   
+   Serial.print("CO: ");Serial.print(iPPM_CO);Serial.println(" ppm");
+   Serial.print("Smoke: ");Serial.print(iPPM_Smoke);Serial.println(" ppm");
+   
+  // Checks if it has reached the threshold value
+//  if (analogSensor > sensorThres)
+//  {
+//    digitalWrite(redLed, HIGH);
+//    digitalWrite(greenLed, LOW);
+//    tone(buzzer, 1000, 200);
+//  }
+//  else
+//  {
+//    digitalWrite(redLed, LOW);
+//    digitalWrite(greenLed, HIGH);
+//    noTone(buzzer);
+//  }
+  
+  return String();
 }
 
 String readDHTPressure() {
@@ -1224,7 +1387,10 @@ String readDHTPressure() {
 // Replaces placeholder with LED state value
 String processor(const String& var){
   Serial.println(var);
-  if(var == "TEMPERATURE"){
+  if(var == "MQ2"){
+    return readMq2();
+  }
+  else if(var == "TEMPERATURE"){
     return readDHTTemperature();
   }
   else if(var == "HUMIDITY"){
@@ -1252,8 +1418,13 @@ void setup(){
  
   //call begin to start sensor
   dht.begin();
-
+  
+  //pinMode(smokeA0, INPUT); // MQ-2 analog pin
   pinMode(ledPin, OUTPUT); // simple led initiation
+
+  Ro = MQCalibration(MQ_PIN);                         //Calibrating the sensor. Please make sure the sensor is in clean air        
+  Serial.println("Done calibrating MQ-2, Ro: ");
+  Serial.println(Ro);
   
   // Initialize SPIFFS
   if(!SPIFFS.begin()){
@@ -1308,7 +1479,9 @@ void setup(){
   server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send_P(200, "text/plain", readDHTHumidity().c_str());
   });
-  
+  server.on("/mq2", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", readMq2().c_str());
+  });
   // Start server
   server.begin();
 }
